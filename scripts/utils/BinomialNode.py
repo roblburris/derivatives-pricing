abaab7648f3aed149e687d2b649998398741d35c
Robert Burris <roblburris@gmail.com>
2020-04-08 00:58:56 -0700

    deleted incorrect BinomialNode

--- scripts/utils/BinomialNode.py
+++
@@ -1,5 +0,0 @@
-
-class BinomialNode():
-    def __init__(self, root, left_node = None, right_node = None):
-        if left_node is not None and right_node is not None:
-            self.root = root
