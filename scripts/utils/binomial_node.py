4fbd2dd315c8135199f525a78bb802759d531208
Robert Burris <roblburris@gmail.com>
2020-04-18 01:10:54 -0700

    working binomial_tree creation

--- scripts/utils/binomial_node.py
+++ scripts/utils/binomial_node.py
@@ -1,6 +1,15 @@
 class BinomialNode():
-    def __init__(self, root, left_node = None, right_node = None):
-        self.root = root
+    """
+    Definition of BinomialNode, used in a binomial pricing tree
+    """
+    def __init__(self, data, left_node = None, right_node = None):
+        """
+        Creates a BinomialNode
+        :param data: float stored in this BinomialNode
+        :param left_node: reference to another BinomialNode, set to null if not specified
+        :param right_node: reference to another BinomialNode, set to null if not specified
+        """
+        self.root = data
         if left_node is not None and right_node is not None:
             self.left_node = left_node
             self.right_node = right_node
--- scripts/utils/binomial_pricing.py
+++ scripts/utils/binomial_pricing.py
@@ -1,4 +1,34 @@
+import numpy as np
 from binomial_node import BinomialNode
 
-class BinomialPricing():
-    def __init__(self, ):
+def binomial_pricing(spot, strike, dividend_yield, volatility, desired_length, len_step):
+    """
+    Generates a binomial price tree
+    """
+    
+    increase_factor = np.e ** (volatility * np.sqrt(len_step))
+    decrease_factor = 1.0 / increase_factor
+
+    def generate_tree(node, spot, time):
+        """
+        Generates binomial price tree two levels at a time using recursion
+        :param node: reference to BinomialNode
+        :param spot: current price at the level
+        :param time: current level the tree is on (time period of level is decided earlier)  
+        """
+        node.left_node = BinomialNode(spot * decrease_factor)
+        node.right_node = BinomialNode(spot * increase_factor)
+        
+        if time + 1 == desired_length:
+            node.left_node.left_node = BinomialNode(spot * decrease_factor * decrease_factor)
+            node.left_node.right_node = BinomialNode(spot * decrease_factor * increase_factor)
+            node.right_node.left_node = node.left_node.right_node
+            node.right_node.right_node = BinomialNode(spot * increase_factor * increase_factor)
+        else:
+            node.left_node.left_node = generate_tree(BinomialNode(spot * decrease_factor * decrease_factor), spot * decrease_factor * decrease_factor, time + 2)
+            node.left_node.right_node = generate_tree(BinomialNode(spot * decrease_factor * increase_factor), spot * decrease_factor * increase_factor, time + 2)
+            node.right_node.left_node = node.left_node.right_node
+            node.right_node.right_node = generate_tree(BinomialNode(spot * increase_factor * increase_factor), spot * increase_factor * increase_factor, time + 2)
+            
+        return node
+    
